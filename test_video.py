e9672b2dec0e6d688d70c2204f708503de5f4807
Shreeya <shreeya12shrestha@email.com>
2026-06-24 21:49:44 +0545

    Weapon detection completed

--- test_video.py
+++ test_video.py
@@ -1,14 +1,16 @@
 import cv2
 from ultralytics import YOLO
 webcam=cv2.VideoCapture(0)
-model= YOLO("yolov8s.pt")
+general_model=YOLO("yolov8s.pt")
+weapon_model= YOLO("best.pt")
 while True:
     ret,img=webcam.read()
     if not ret:
         break
     img=cv2.flip(img,1)
-    results=model(img, classes=[0], conf=0.5, verbose=False)
-    img=results[0].plot()
+    img=general_model(img, classes=[0], conf=0.5, verbose=False)[0].plot()
+    img=weapon_model(img, classes=[0], conf=0.5, verbose=False)[0].plot()
+    
     cv2.imshow("Surveillance System",img)
     key=cv2.waitKey(1)
     if key==27:
