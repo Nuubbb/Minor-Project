ee9fa8025ce137396ced07291ff09dba5bf873ad
Nuubbb <samip5327@gmail.com>
2026-06-26 00:37:00 +0545

    Changes
    Refactor video processing code into a function and add alert system for weapon detection.

--- test_video.py
+++ test_video.py
@@ -1,19 +1,23 @@
 import cv2
 from ultralytics import YOLO
-webcam=cv2.VideoCapture(0)
-general_model=YOLO("yolov8s.pt")
-weapon_model= YOLO("best.pt")
-while True:
-    ret,img=webcam.read()
-    if not ret:
-        break
-    img=cv2.flip(img,1)
-    img=general_model(img, classes=[0], conf=0.5, verbose=False)[0].plot()
-    img=weapon_model(img, classes=[0], conf=0.5, verbose=False)[0].plot()
+
+general_model = YOLO("yolov8s.pt")
+weapon_model = YOLO("best.pt")
+
+def process_frame(img):
+
+    img = cv2.flip(img, 1)
     
-    cv2.imshow("Surveillance System",img)
-    key=cv2.waitKey(1)
-    if key==27:
-        break
-webcam.release()
-cv2.destroyAllWindows()
+    img = general_model(img, classes=[0], conf=0.5, verbose=False)[0].plot()
+    
+    weapon_results = weapon_model(img, classes=[0], conf=0.5, verbose=False)[0]
+    img = weapon_results.plot()
+    
+    alert_triggered = False
+    alert_message = ""
+    
+    if len(weapon_results.boxes) > 0:
+        alert_triggered = True
+        alert_message = "CRITICAL: Threat Weapon Spotted!"
+        
+    return img, alert_triggered, alert_message
